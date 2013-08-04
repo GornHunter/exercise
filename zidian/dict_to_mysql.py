42343506dca72c4df857e3b2798c8e5ad64aba6f
Aimei Yan <aimeiyan@gmail.com>
2013-08-04 23:35:12 +0800

    test dict to mysql

--- zidian/dict_to_mysql.py
+++ zidian/dict_to_mysql.py
@@ -5,26 +5,32 @@ import MySQLdb
 db = MySQLdb.connect(db="exercise5", user="root", host="localhost")
 db.autocommit(True)
 c = db.cursor()
-c.execute("DROP TABLE IF EXISTS WORDDICT")
-c.execute("CREATE TABLE WORDDICT(Id INT PRIMARY KEY AUTO_INCREMENT, \
-                 Name VARCHAR(25))")
+c.execute("DROP TABLE IF EXISTS worddict")
+c.execute("""CREATE TABLE worddict(id INT PRIMARY KEY AUTO_INCREMENT,
+                                   name varchar(128) not null,
+                                   meaning mediumtext)""")
 
 SAVE_DICT = '/home/nancy/Downloads/worddictionary'
 word = eval(open(SAVE_DICT, 'r').read())
-print word
+print word.keys()[:10]
 
+
+import json
 for key, value in word.items():
-    id=key
-    c.execute("insert into WORDDICT (id) values (%s)" % id)
-    for midkey, midvalue in value.items():
-        if midvalue:
-            query = "update WORDDICT set %s ='%s'"
-            c.execute(query % (midkey, midvalue))
-            exa = value['example']
-            for eachmeaning in value['examples']:
-                for finalkey, finalvalue in exa:
-                    if finalvalue:
-                        lean = "update WORDDICT set %s='%s'"
-                        c.execute(lean % (finalkey, finalvalue))
+    # str(value) => "abc"
+    c.execute("insert into worddict(name, meaning) values (%s, %s)", (key, json.dumps(value)))
+    #
+    # id=key
+    # c.execute("insert into worddict (id) values (%s)" % id)
+    # for midkey, midvalue in value.items():
+    #     if midvalue:
+    #         query = "update worddict set %s ='%s'"
+    #         c.execute(query % (midkey, midvalue))
+    #         exa = value['example']
+    #         for eachmeaning in value['examples']:
+    #             for finalkey, finalvalue in exa:
+    #                 if finalvalue:
+    #                     lean = "update worddict set %s='%s'"
+    #                     c.execute(lean % (finalkey, finalvalue))
 c.close()
 db.commit()
