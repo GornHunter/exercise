ea3750a20132411ab7c82e2be7cbd598286ed4fb
Aimei Yan <aimeiyan@gmail.com>
2013-08-05 22:56:19 +0800

    get data from mysql into website

---
+++ tornado/zidian_tonado.py
@@ -0,0 +1,51 @@
+# -*- coding:utf-8 -*-
+
+__author__ = 'nancy'
+
+import tornado.web
+import tornado.ioloop
+import MySQLdb
+
+# w = {'spice': 'n.香料，调味品；香气，香味；趣味，情趣；少许', 'posh': 'adj.精美的；豪华的；漂亮的；时髦的', 'cocktail': 'n.鸡尾酒；餐前开胃菜；混合物'}
+default = {'title': 'My title', 'meaning': 'No exit'}
+
+db = MySQLdb.connect(db="exercise5", user="root", host="localhost")
+db.autocommit(True)
+c = db.cursor()
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        v = default.copy()
+        v['meaning'] = "Please Search"
+        self.render("index.html", **v)
+
+
+class MeanHandler(tornado.web.RequestHandler):
+    def get(self):
+        word = self.get_argument('k')
+        v = default.copy()
+        # if word in w:
+        v['meaning'] = c.execute("select meaning from worddict where name=%s", word)
+        # print c.fetchone()[0]
+        p=c.fetchone()
+        if p:
+            v['meaning'] = p[0]
+        else:
+            v['meaning']='No Exit'
+
+        self.render("index.html", **v)
+
+        # else:
+        # # v['meaning'] = "No Exit"
+        # self.render("index.html", **v)
+
+
+application = tornado.web.Application([
+    (r"/", MainHandler),
+    (r'/word', MeanHandler)
+])
+
+if __name__ == '__main__':
+    application.listen(9090)
+    tornado.ioloop.IOLoop.instance().start()
