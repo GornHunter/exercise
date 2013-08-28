7f84e8dc36a0476d99f6f9fdc636b173e59c0fef
Aimei Yan <aimeiyan@gmail.com>
2013-08-29 07:26:41 +0800

    hashing_cookie

--- web_develop_test/cookie_test.py
+++ web_develop_test/cookie_test.py
@@ -2,5 +2,20 @@ __author__ = 'nancy'
 
 import os
 import webapp2
+import jinja2
 
-from 
+from google.appengine.ext import webapp
+
+from google.appengine.ext.webapp import util
+
+class HwHandler(webapp.RequestHandler):
+    def get(self):
+        self.response.out.write('Hi there!')
+def main():
+    application = webapp.WSGIApplication([('/', HwHandler)],
+                                         debug=True)
+    util.run_wsgi_app(application)
+
+
+if __name__ == '__main__':
+    main()
--- web_develop_test/password_hashing.py
+++ web_develop_test/password_hashing.py
@@ -5,12 +5,21 @@ import string
 import hashlib
 
 def make_salt():
-    return ''.join(random.choice(string.letters) for x in xrange(5))
+    return ''.join(random.choice(string.letters) for x in range(5))
 
 print make_salt()
 
-def make_new_hash(name,pw):
-    salt=make_salt()
+def make_new_hash(name,pw,salt=None):
+    if not salt:
+        salt=make_salt()
     h=hashlib.sha256(name+pw+salt).hexdigest()
     return '%s,%s'%(h,salt)
+
+def valid_pw(name,pw,h):
+    salt=h.split(',')[1]
+    return h==make_new_hash(name,pw,salt)
+
+h=make_new_hash('spez','hunter2')
+print h
+print valid_pw('spez','hunter2',h)
 print make_new_hash('spez','hunter2')
