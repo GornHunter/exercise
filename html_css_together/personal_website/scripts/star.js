48a3bcc3832b630657b694a0ef1c88327044c08d
Aimei Yan <aimeiyan@gmail.com>
2013-12-23 21:57:19 +0800

    two month code

---
+++ html_css_together/personal_website/scripts/star.js
@@ -0,0 +1,43 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: nancy
+ * Date: 12/21/13
+ * Time: 10:35 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+$(function () {
+//    $("ul.selectScore li a").click(function(){
+//        var title=$(this).attr("title");
+//        alert("您给此产品的评分为："+title);
+//        var cl=$(this).parent().attr("class")
+//        $(this).parent().parent().removeClass().addClass("selectScore "+cl+"star");
+//        return false
+//    })
+
+
+    $(document).mousemove(function(e){
+//        console.log(e.clientX, e.clientY, e.pageX, e.pageY);
+    });
+
+    var a = "'abbc'\"\t\n";
+
+    var select_idx = -1;
+
+    $("ul.selectScore li").hover(function () {
+        var index = $("ul.selectScore li").index(this);
+        var y = index * (-16) - 96 + "px";
+        $("ul.selectScore").css("background-position-y", y)
+    },function () {
+        var y = select_idx * (-16) - 16 + "px";
+        $("ul.selectScore").css("background-position-y", y);
+    }).click(function () {
+            var idx = $("ul.selectScore li").index(this);
+            select_idx = idx;
+            var y = idx * (-16) - 16 + "px";
+            $("ul.selectScore").css("background-position-y", y);
+            var title = $(this).attr("title");
+            alert("您给此产品的评分为：" + title);
+            return false
+        });
+});
